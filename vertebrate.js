1dce44aa590cf7b51e321492569d6ba0059e97b0
psalmody <psalmody@gmail.com>
2015-07-06 08:13:12 -0800

    Working out extend logic

--- vertebrate.js
+++ vertebrate.js
@@ -2,27 +2,52 @@ if (typeof($) == 'undefined') {
     console.log('Vertebrate.js requires jQuery 1.11+');
 }
 
+/**
+*
+
+In progress:
+* add change events for listening with $ on
+* add changed attribute to mark what keys changed
+* add delete / remove functions
+
+*change back to functions... extend needs to return the function so we can
+  use new Modelname() to construct new models
+
+*
+*/
+
 var Vertebrate = {
+    settings: {},
     get: function( setting ) {
-        return this.settings[setting];
+        return (typeof(setting)) == 'undefined' ?
+         this.settings :
+         this.settings[setting];
+    },
+    set: function( setting, value ) {
+        return this.settings[setting] = value;
+    },
+    shared: {
+        fn: {
+            attributes: {},
+            changedattrs: [],
+            set: function( attr, value ) {
+                this.attributes[attr] = value;
+                return true;
+            },
+            get: function( attr ) {
+                if (typeof(attr) == 'undefined') return this.attributes;
+                return this.attributes[attr];
+            },
+            has: function( attr ) {
+                return (typeof(this.attributes[attr]) == 'undefined') ? false : true;
+            }
+        }
     }
 };
 
-Vertebrate.settings = {
-    url: 'json/test.json'
-}
-
 Vertebrate.Model = function( options ) {
     var self = this;
-    this.attributes = $.extend({}, options);
-    this.set = function( attr, value ) {
-        this.attributes[attr] = value;
-        return true;
-    };
-    this.get = function( attr ) {
-        if (typeof(attr) == 'undefined') return this.attributes;
-        return this.attributes[attr];
-    };
+    $.extend(this,Vertebrate.shared.fn,options);
     this.save = function( callback ) {
         var promise = $.ajax({
             url: typeof(this.get('url')) == 'undefined' ? Vertebrate.get('url') : this.get('url'),
@@ -44,23 +69,21 @@ Vertebrate.Model = function( options ) {
             }
         })
         return promise;
-    }
-    return this;
+    };
 };
 
-Vertebrate.Collection = function( options ) {
-    var self = this;
-    this.models = typeof(this.models) == 'undefined' ? [] : this.models;
-    this.attributes = $.extend({},options);
-    this.set = function( attr, value ) {
-        this.attributes[attr] = value;
-        return true;
-    };
-    this.get = function( attr ) {
-        if (typeof(attr) == 'undefined') return this.attributes;
-        return this.attributes[attr];
-    };
-    this.save = function( callback ) {
+Vertebrate.Model.Extend = function( options ) {
+    return $.extend(Vertebrate.Model,options);
+}
+
+Vertebrate.Collection = {
+    Extend: function( options ) {
+        var collection = $.extend({},Vertebrate.shared.fn,this,options);
+        delete collection.Extend;
+        return collection;
+    },
+    models: [],
+    save: function( callback ) {
         var promise = $.ajax({
             url: typeof(this.get('url')) == 'undefined' ? Vertebrate.get('url') : this.get('url'),
             method: 'POST',
@@ -70,8 +93,8 @@ Vertebrate.Collection = function( options ) {
             }
         });
         return promise;
-    };
-    this.fetch = function( callback ) {
+    },
+    fetch: function( callback ) {
         var promise = $.ajax({
             url: typeof(this.get('url')) == 'undefined' ? Vertebrate.get('url') : this.get('url'),
             method: 'GET',
@@ -85,11 +108,10 @@ Vertebrate.Collection = function( options ) {
             }
         })
         return promise;
-    };
-    this.find = function( term, attr ) {
+    },
+    find: function( term, attr ) {
         var attr = typeof(attr) == 'undefined' ? false : attr;
         var found = false;
-
         if (attr) {
             var founds = [];
             $.each(this.models,function(k,v) {
@@ -110,6 +132,5 @@ Vertebrate.Collection = function( options ) {
             return found;
         }
 
-    };
-    this.find
+    },
 };
