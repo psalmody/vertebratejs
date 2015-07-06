e9ec9683563d74073eac642ada4f30ced2659afa
psalmody <psalmody@gmail.com>
2015-07-06 12:32:35 -0800

    basic functionality working
    Have at least the basic functionality for the toolkit.

--- vertebrate.js
+++ vertebrate.js
@@ -31,7 +31,11 @@ var Vertebrate = {
             attributes: {},
             changedattrs: [],
             set: function( attr, value ) {
+                var self = this;
                 this.attributes[attr] = value;
+                this.changedattrs.push(attr);
+                console.log('setting');
+                $('body').trigger('vertebrate.changeattr',[self,self.attributes,self.changedattrs]);
                 return true;
             },
             get: function( attr ) {
@@ -40,6 +44,13 @@ var Vertebrate = {
             },
             has: function( attr ) {
                 return (typeof(this.attributes[attr]) == 'undefined') ? false : true;
+            },
+            hasChanged: function( attr ) {
+                var attr = typeof(attr) == 'undefined' ? false : attr;
+                if (!attr) {
+                    return this.changedattrs.length > 0 ? true : false;
+                }
+                return this.changedattrs.indexOf(attr) >= 0 ? true : false;
             }
         }
     }
@@ -54,6 +65,7 @@ Vertebrate.Model = function( options ) {
             method: 'POST',
             data: this.get(),
             success: function( data, status, xhr ) {
+                self.changedattrs = [];
                 if (typeof(callback) == 'function') callback( data, status, xhr );
             }
         });
@@ -65,6 +77,7 @@ Vertebrate.Model = function( options ) {
             method: 'GET',
             data: this.get(),
             success: function( data, status, xhr ) {
+                self.changedattrs = [];
                 if (typeof(callback) == 'function') callback( data, status, xhr );
             }
         })
@@ -73,17 +86,23 @@ Vertebrate.Model = function( options ) {
 };
 
 Vertebrate.Model.Extend = function( options ) {
-    return $.extend(Vertebrate.Model,options);
+    function model( opts ) {
+        Vertebrate.Model.call(this,options);
+        $.extend(this,opts);
+    }
+    model.prototype = Object.create(Vertebrate.Model.prototype);
+    model.prototype.constructor = model;
+    return model;
 }
 
-Vertebrate.Collection = {
-    Extend: function( options ) {
-        var collection = $.extend({},Vertebrate.shared.fn,this,options);
-        delete collection.Extend;
-        return collection;
-    },
-    models: [],
-    save: function( callback ) {
+Vertebrate.Collection = function(options) {
+    this.model = false;
+    this.models = [];
+    this.added = [];
+    this.removed = [];
+    var self = this;
+    $.extend(this,Vertebrate.shared.fn,options);
+    this.save = function( callback ) {
         var promise = $.ajax({
             url: typeof(this.get('url')) == 'undefined' ? Vertebrate.get('url') : this.get('url'),
             method: 'POST',
@@ -93,8 +112,12 @@ Vertebrate.Collection = {
             }
         });
         return promise;
-    },
-    fetch: function( callback ) {
+    };
+    this.fetch = function( callback ) {
+        if (!self.model) {
+            console.log('No model defined. Models must be defined before fetch call.');
+            return false;
+        }
         var promise = $.ajax({
             url: typeof(this.get('url')) == 'undefined' ? Vertebrate.get('url') : this.get('url'),
             method: 'GET',
@@ -102,14 +125,14 @@ Vertebrate.Collection = {
             success: function( data, status, xhr ) {
                 self.models = [];
                 $.each(data,function(i) {
-                    self.models.push(new Vertebrate.Model(this));
+                    self.models.push(new self.model({"attributes":data[i]}));
                 })
                 if (typeof(callback) == 'function') callback( data, status, xhr );
             }
         })
         return promise;
-    },
-    find: function( term, attr ) {
+    };
+    this.find = function( term, attr ) {
         var attr = typeof(attr) == 'undefined' ? false : attr;
         var found = false;
         if (attr) {
@@ -123,7 +146,7 @@ Vertebrate.Collection = {
             if (!found) return false;
             return founds;
         } else {
-            $.each(this.models,function(k,v) {
+            $.each(self.models,function(k,v) {
                 if (k == term) {
                     found = this;
                     return false;
@@ -131,6 +154,29 @@ Vertebrate.Collection = {
             });
             return found;
         }
-
-    },
+    };
+    this.add = function( model ) {
+        self.models.push( model );
+        self.added.push( model );
+    };
+    this.remove = function( model ) {
+        if (self.models.indexOf(model) == -1) {
+            return false;
+        }
+        self.removed.push(model);
+        self.models = self.models.filter(function() {
+            return this != model;
+        });
+        return true;
+    }
 };
+
+Vertebrate.Collection.Extend = function( options ) {
+    function collection( opts ) {
+        Vertebrate.Collection.call(this,options);
+        $.extend(this,opts);
+    }
+    collection.prototype = Object.create(Vertebrate.Collection.prototype);
+    collection.prototype.constructor = collection;
+    return collection;
+}
